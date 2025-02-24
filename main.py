1ab4ec227e75441d1962d9a72edb9b3b66c62e59
Celdan J T <ctibay580@gmaail.com>
2025-02-23 21:37:02 -0800

    commiting

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ def main():
     print('Hello World')
     print('CS140: Python Programming')
 
-if __name__ == '__main__':  # Double underscores __ on both sides
+if __name__ == '__main__':  # Double underscores on both sides
     main()
